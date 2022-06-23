a412219a03afc9a5ce7fdc5ce8b91de79a6fbb96
studyoutube <repostudyoutube@outlook.com>
2022-06-23 13:56:20 -0300

    home page end

--- frontend/src/app/services/moment.service.ts
+++ frontend/src/app/services/moment.service.ts
@@ -11,12 +11,12 @@ import { Response } from '../Response';
 })
 export class MomentService {
   private baseApiUrl = environment.baseApiUrl;
-  private apiUrl = `${this.baseApiUrl}/api/moments`;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
 
-  getMoments(): Observable<Response<Moment[]>>{
-    return this.http.get<Response<Moment[]>>(this.apiUrl)
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
   }
 
   createMoment(formData: FormData): Observable<FormData> {
